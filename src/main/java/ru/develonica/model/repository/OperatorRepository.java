8598179d9213c6734e776044cf874f66b224a3d1
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-05 02:26:45 +0400

    Добавлен репозиторий для операторов

---
+++ src/main/java/ru/develonica/model/repository/OperatorRepository.java
@@ -0,0 +1,7 @@
+package ru.develonica.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.develonica.model.mapper.OperatorMapper;
+
+public interface OperatorRepository extends JpaRepository<OperatorMapper, Long> {
+}
