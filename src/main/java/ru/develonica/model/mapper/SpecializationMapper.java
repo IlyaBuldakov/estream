20bd6c6a444ec5afda62b775ce8364836569d6c3
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-05 02:09:19 +0400

    Добавлена модель специализации

---
+++ src/main/java/ru/develonica/model/mapper/SpecializationMapper.java
@@ -0,0 +1,33 @@
+package ru.develonica.model.mapper;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+
+/**
+ * Модель специализации.
+ */
+@Entity
+@Table(name = "specialization")
+public class SpecializationMapper {
+
+    /**
+     * Идентификатор.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "specialization_id")
+    @Getter
+    private Long id;
+
+    /**
+     * Наименование специализации.
+     */
+    @Column(name = "specialization_name")
+    @Getter
+    private String name;
+}
