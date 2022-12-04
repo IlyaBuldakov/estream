2b3ab03976a696113fc568d89825154688ea5fae
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-05 02:09:31 +0400

    Добавлен репозиторий специализаций

---
+++ src/main/java/ru/develonica/model/repository/SpecializationRepository.java
@@ -0,0 +1,7 @@
+package ru.develonica.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.develonica.model.mapper.SpecializationMapper;
+
+public interface SpecializationRepository extends JpaRepository<SpecializationMapper, Long> {
+}
