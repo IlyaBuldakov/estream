3ed3dc7669f3ac42f02d9a4992b8e291b752aff1
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-05 02:24:36 +0400

    Добавлена конфигурация Spring Security

---
+++ src/main/java/ru/develonica/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package ru.develonica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] SECURED_PATHS = new String[]{
+            "/panel",
+            "/admin"
+    };
+
+    private static final String LOGOUT_URL = "/logout";
+
+    private static final String LOGIN_URL = "/login";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests(config -> config
+                        .antMatchers(SECURED_PATHS).authenticated()
+                        .anyRequest().permitAll())
+                .logout(config -> config
+                        .logoutUrl(LOGOUT_URL).permitAll()
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
+                .formLogin()
+                .loginPage(LOGIN_URL)
+                .and()
+                .httpBasic();
+    }
+}
