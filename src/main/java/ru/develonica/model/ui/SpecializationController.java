bfd94372f6fda258e95b915e2dfc21f5a9548a03
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-08 22:32:22 +0400

    Добавлен UI (JSF) контроллер специализаций

---
+++ src/main/java/ru/develonica/model/ui/SpecializationController.java
@@ -0,0 +1,49 @@
+package ru.develonica.model.ui;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import ru.develonica.model.mapper.SpecializationMapper;
+import ru.develonica.model.repository.SpecializationRepository;
+
+@ManagedBean(name = "specializationController")
+@SessionScoped
+@Component
+public class SpecializationController implements Serializable {
+
+    private List<SpecializationMapper> specializationsList;
+
+    private final SpecializationRepository specializationRepository;
+
+    @Getter
+    private boolean specializationChosen;
+
+    @Getter
+    private String activeSpecializationName;
+
+    public SpecializationController(List<SpecializationMapper> specializationsList,
+                                    SpecializationRepository specializationRepository) {
+        this.specializationsList = specializationsList;
+        this.specializationRepository = specializationRepository;
+    }
+
+    public List<SpecializationMapper> getSpecializationsList() {
+        this.specializationsList = this.specializationRepository.findAll();
+        return this.specializationsList;
+    }
+
+    public String chooseSpecialization(SpecializationMapper specialization) {
+        this.specializationChosen = true;
+        this.activeSpecializationName = specialization.toString();
+        return "specializations.xhtml";
+    }
+
+    public String cancelChoose() {
+        this.specializationChosen = false;
+        this.activeSpecializationName = null;
+        return "specializations.xhtml";
+    }
+}
