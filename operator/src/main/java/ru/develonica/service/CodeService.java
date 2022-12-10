a97cc4cf05cd2738700bf27100784c68ca8bda35
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-11 02:32:27 +0400

    Добавлен сервис для работы с кодом в очереди

---
+++ operator/src/main/java/ru/develonica/service/CodeService.java
@@ -0,0 +1,40 @@
+package ru.develonica.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.stereotype.Service;
+import ru.develonica.model.mapper.CodeMapper;
+import ru.develonica.repository.CodeRepository;
+
+@Service
+public class CodeService {
+
+    private final static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    private final DataSource dataSource;
+
+    private final CodeRepository codeRepository;
+
+    public CodeService(DataSource dataSource, CodeRepository codeRepository) {
+        this.dataSource = dataSource;
+        this.codeRepository = codeRepository;
+    }
+
+    public void generateSequence(String specializationName) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String query = "call generate_sequence(?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, specializationName);
+        preparedStatement.execute();
+
+        createCodeMapping(specializationName);
+    }
+
+    private void createCodeMapping(String specializationName) {
+        double randomDouble = Math.random() / Math.nextDown(1.0);
+        int randomIntResult = (int) ((1.0 - randomDouble) + ALPHABET.length * randomDouble);
+        codeRepository.save(new CodeMapper(ALPHABET[randomIntResult], specializationName));
+    }
+}
