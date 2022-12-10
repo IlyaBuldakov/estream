592b1d783253a4cae629106107d6c15755a6c13b
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-11 01:30:02 +0400

    SpecializationRepository вынесен в общий модуль

--- common/src/main/java/ru/develonica/model/repository/SpecializationRepository.java
+++ common/src/main/java/ru/develonica/model/repository/SpecializationRepository.java
@@ -1,7 +1,6 @@
 package ru.develonica.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.develonica.model.mapper.SpecializationMapper;
 
 public interface SpecializationRepository extends JpaRepository<SpecializationMapper, Long> {
