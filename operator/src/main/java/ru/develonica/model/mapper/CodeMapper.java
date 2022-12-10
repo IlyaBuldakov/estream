327e58f9749e0b0fa5fdc8c2b31c666c8e91b4c9
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-11 02:31:53 +0400

    Добавлена модель маппера кода, выдаваемого пользователю в очереди

---
+++ operator/src/main/java/ru/develonica/model/mapper/CodeMapper.java
@@ -0,0 +1,40 @@
+package ru.develonica.model.mapper;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "code")
+public class CodeMapper {
+
+    public CodeMapper() {
+    }
+
+    public CodeMapper(char codeLetter, String sequenceName) {
+        this.codeLetter = codeLetter;
+        this.sequenceName = sequenceName;
+    }
+
+    @Id
+    @Column(name = "code_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code_letter")
+    private char codeLetter;
+
+    @Column(name = "sequence_name")
+    private String sequenceName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
