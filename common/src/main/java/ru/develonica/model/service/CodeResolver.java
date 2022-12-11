51ded9eec70b7368ff5d0e2fcaa2b94af228b4dd
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-11 17:26:12 +0400

    Добавлен преобразователь кода очереди

---
+++ common/src/main/java/ru/develonica/model/service/CodeResolver.java
@@ -0,0 +1,32 @@
+package ru.develonica.model.service;
+
+import java.math.BigInteger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Component;
+import ru.develonica.model.mapper.CodeMapper;
+
+@Component
+public class CodeResolver {
+
+    private static final String LAST_SEQ_ELEM_QUERY = "SELECT last_value FROM \"%s\"";
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    public CodeResolver(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public String resolve(CodeMapper codeMapper) {
+        StringBuilder sb = new StringBuilder();
+        String sql = String.format(LAST_SEQ_ELEM_QUERY, codeMapper.getSequenceName());
+        Query query = entityManager.createNativeQuery(sql);
+        BigInteger result = (BigInteger) query.getSingleResult();
+
+        sb.append(Character.toUpperCase(codeMapper.getCodeLetter()));
+        sb.append(result);
+        return sb.toString();
+    }
+}
