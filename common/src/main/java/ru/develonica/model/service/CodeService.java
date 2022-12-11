eaa5822ad87ed1fbe6045e7acf12c7d8b5fdf29f
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-11 17:35:29 +0400

    Исправлено: применен EntityManager

--- common/src/main/java/ru/develonica/model/service/CodeService.java
+++ common/src/main/java/ru/develonica/model/service/CodeService.java
@@ -1,9 +1,10 @@
 package ru.develonica.model.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import ru.develonica.model.mapper.CodeMapper;
 import ru.develonica.model.repository.CodeRepository;
@@ -11,24 +12,25 @@ import ru.develonica.model.repository.CodeRepository;
 @Service
 public class CodeService {
 
-    private final static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
-    private final DataSource dataSource;
+    private static final String CALL_SEQ_GENERATION_PROCEDURE_QUERY = "call generate_sequence(?)";
+
+    @PersistenceContext
+    private final EntityManager entityManager;
 
     private final CodeRepository codeRepository;
 
-    public CodeService(DataSource dataSource, CodeRepository codeRepository) {
-        this.dataSource = dataSource;
+    public CodeService(EntityManager entityManager, CodeRepository codeRepository) {
+        this.entityManager = entityManager;
         this.codeRepository = codeRepository;
     }
 
+    @Transactional
     public void generateCode(String specializationName) throws SQLException {
-        Connection connection = dataSource.getConnection();
-        String query = "call generate_sequence(?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setString(1, specializationName);
-        preparedStatement.execute();
-
+        Query query = entityManager.createNativeQuery(CALL_SEQ_GENERATION_PROCEDURE_QUERY);
+        query.setParameter(1, specializationName);
+        query.executeUpdate();
         createCodeMapping(specializationName);
     }
 
