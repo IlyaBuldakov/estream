520e8bd3cf22c146845dd040dceeaf904514f6db
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-12 23:07:56 +0400

    Исправлен идентификатор в репозитории для работы с очередями

--- user/src/main/java/ru/develonica/model/repository/QueueRepository.java
+++ user/src/main/java/ru/develonica/model/repository/QueueRepository.java
@@ -1,10 +1,11 @@
 package ru.develonica.model.repository;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.develonica.model.mapper.QueueMapper;
 
 /**
  * Репозиторий для работы с очередями.
  */
-public interface QueueRepository extends JpaRepository<QueueMapper, Long> {
+public interface QueueRepository extends JpaRepository<QueueMapper, UUID> {
 }
