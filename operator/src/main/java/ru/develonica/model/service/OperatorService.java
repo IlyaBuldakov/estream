04be73ac918d8d674ea106ee872c2869cddf8581
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-12 23:09:53 +0400

    Добавлен метод установки UUID пользователя для оператора

--- operator/src/main/java/ru/develonica/model/service/OperatorService.java
+++ operator/src/main/java/ru/develonica/model/service/OperatorService.java
@@ -1,7 +1,10 @@
 package ru.develonica.model.service;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import ru.develonica.model.Operator;
 import ru.develonica.model.mapper.OperatorMapper;
 import ru.develonica.model.mapper.SpecializationMapper;
 import ru.develonica.model.repository.OperatorRepository;
@@ -52,5 +55,20 @@ public class OperatorService {
                 .getAuthentication()
                 .getPrincipal())
                 .getUsername();
+    }
+
+    /**
+     * Метод установки UUID пользователя, которого обслуживает оператор.
+     *
+     * @param operator Оператор, чьё поле будем устанавливать.
+     * @param currentUserUUID UUID пользователя.
+     */
+    public void setUserUuid(Operator operator, UUID currentUserUUID) {
+        Optional<OperatorMapper> operatorOptional = operatorRepository.findById(operator.getId());
+        if (operatorOptional.isPresent()) {
+            OperatorMapper operatorFromDb = operatorOptional.get();
+            operatorFromDb.setUserUUID(currentUserUUID);
+            operatorRepository.save(operatorFromDb);
+        }
     }
 }
