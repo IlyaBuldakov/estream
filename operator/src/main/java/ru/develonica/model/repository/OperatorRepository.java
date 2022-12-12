8f092d0f2575194fb79602d8f6f82d24778d17bf
IlyaBuldakov <ggpanguin@gmail.com>
2022-12-12 17:51:05 +0400

    Добавлен метод в JPA репозиторий для поиска оператора по специализациям

--- operator/src/main/java/ru/develonica/model/repository/OperatorRepository.java
+++ operator/src/main/java/ru/develonica/model/repository/OperatorRepository.java
@@ -1,12 +1,16 @@
 package ru.develonica.model.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.develonica.model.mapper.OperatorMapper;
+import ru.develonica.model.mapper.SpecializationMapper;
 
 /**
  * Репозиторий для работы с операторами.
  */
 public interface OperatorRepository extends JpaRepository<OperatorMapper, Long> {
+    List<OperatorMapper> findAllBySpecializationsIn(List<SpecializationMapper> specializations);
 
     OperatorMapper getByEmail(String email);
+
 }
