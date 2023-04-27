ab4397dc195b1d93bf28ee88522b7c3eba6ffaf7
IlyaBuldakov <ggpanguin@gmail.com>
2023-04-27 14:33:13 +0400

    Добавлен контроллер статистики оператора

---
+++ operator/src/main/java/ru/develonica/controller/StatController.java
@@ -0,0 +1,22 @@
+package ru.develonica.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Контроллер статистики.
+ */
+@Controller
+@RequestMapping("/stat")
+public class StatController {
+
+    /**
+     * Метод получения страницы статистики.
+     * @return Страница статистики.
+     */
+    @GetMapping
+    public String getStat() {
+        return "stat.xhtml";
+    }
+}
