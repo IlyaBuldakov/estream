88b7d466cfd3fc1abe695f4a4383498066bdb8b7
IlyaBuldakov <ggpanguin@gmail.com>
2023-05-05 11:53:08 +0400

    Исправлена настройка Spring Security, дополнен logout

--- operator/src/main/java/ru/develonica/config/SecurityConfig.java
+++ operator/src/main/java/ru/develonica/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 /**
  * Конфигурационный класс Spring Security.
@@ -20,8 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] AUTHENTICATED_PATHS = {PANEL_PATH, "/stat"};
 
-    private static final String LOGOUT_URL = "/logout";
-
     private static final String LOGIN_URL = "/login";
 
     @Override
@@ -31,15 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests(config -> config
                         .antMatchers(AUTHENTICATED_PATHS).authenticated()
                         .anyRequest().permitAll())
-                .logout(config -> config
-                        .logoutUrl(LOGOUT_URL).permitAll()
-                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
                 .formLogin()
                 .loginPage(LOGIN_URL)
                 .loginProcessingUrl(LOGIN_URL)
                 .defaultSuccessUrl(PANEL_PATH)
                 .and()
-                .formLogin()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/")
                 .and()
                 .httpBasic();
     }
