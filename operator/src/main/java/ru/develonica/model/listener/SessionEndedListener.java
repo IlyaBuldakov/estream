4953d69f234261ec3521ec80cc2183ea4cbd2a84
IlyaBuldakov <ggpanguin@gmail.com>
2023-05-06 18:03:02 +0400

    Добавлен слушатель завершения сессии

---
+++ operator/src/main/java/ru/develonica/model/listener/SessionEndedListener.java
@@ -0,0 +1,35 @@
+package ru.develonica.model.listener;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Component;
+import ru.develonica.model.Operator;
+import ru.develonica.model.service.OperatorService;
+import ru.develonica.view.managed.PanelBean;
+
+@Component
+public class SessionEndedListener implements HttpSessionListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PanelBean.class);
+
+    @Autowired
+    private OperatorService operatorService;
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        LOG.info("Сессия %s завершена".formatted(se.getSession().getId()));
+        HttpSession session = se.getSession();
+        SecurityContext context = (SecurityContext) session.getAttribute
+                (HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication = context.getAuthentication();
+        Operator operator = (Operator) authentication.getPrincipal();
+        this.operatorService.disableOperatorWorkSession(operator);
+    }
+}
