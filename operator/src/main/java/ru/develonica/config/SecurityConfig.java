78fd4a3507544b1a64c28b15c54580c91f619e18
IlyaBuldakov <ggpanguin@gmail.com>
2023-05-08 15:21:21 +0400

    Новый провайдер аутентификации настроен в конфигурации Spring Security

--- operator/src/main/java/ru/develonica/config/SecurityConfig.java
+++ operator/src/main/java/ru/develonica/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package ru.develonica.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.develonica.security.ApplicationAuthenticationProvider;
 import ru.develonica.security.OperatorDetailsService;
 
 /**
@@ -56,5 +58,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    protected DaoAuthenticationProvider daoAuthenticationProvider() {
+        ApplicationAuthenticationProvider applicationAuthenticationProvider = new ApplicationAuthenticationProvider();
+        applicationAuthenticationProvider.setUserDetailsService(this.operatorDetailsService);
+        applicationAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return applicationAuthenticationProvider;
     }
 }
